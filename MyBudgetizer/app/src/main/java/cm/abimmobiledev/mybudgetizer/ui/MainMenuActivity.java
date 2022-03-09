158df0f20be6a28ceeae196ea0189c9fb8662d54
AbdoulBasihd <abdulbasihd@gmail.com>
2022-03-09 10:59:33 +0100

    main menu design

---
+++ MyBudgetizer/app/src/main/java/cm/abimmobiledev/mybudgetizer/ui/MainMenuActivity.java
@@ -0,0 +1,16 @@
+package cm.abimmobiledev.mybudgetizer.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cm.abimmobiledev.mybudgetizer.R;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
