12220ea81eee1e6e78dc35ef57898e1717572504
AbdoulBasihd <abdulbasihd@gmail.com>
2022-03-09 15:11:26 +0100

    app main dashboard design

--- MyBudgetizer/app/src/main/java/cm/abimmobiledev/mybudgetizer/ui/MainMenuActivity.java
+++ MyBudgetizer/app/src/main/java/cm/abimmobiledev/mybudgetizer/ui/MainMenuActivity.java
@@ -1,16 +1,37 @@
 package cm.abimmobiledev.mybudgetizer.ui;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import cm.abimmobiledev.mybudgetizer.R;
+import cm.abimmobiledev.mybudgetizer.databinding.ActivityMainMenuBinding;
+import cm.abimmobiledev.mybudgetizer.nav.ExNavigation;
+import cm.abimmobiledev.mybudgetizer.useful.Util;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    ActivityMainMenuBinding mainMenuBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        mainMenuBinding = DataBindingUtil.setContentView(this, R.layout.activity_main_menu);
+
+        //implement this group first....
+        mainMenuBinding.cardBudget.setOnClickListener(aboutView -> Snackbar.make(aboutView, getString(R.string.not_yet_implemented), Snackbar.LENGTH_LONG).show());
+        mainMenuBinding.cardDebts.setOnClickListener(aboutView -> Snackbar.make(aboutView, getText(R.string.not_yet_implemented), Snackbar.LENGTH_LONG).show());
+        mainMenuBinding.cardReceivable.setOnClickListener(aboutView -> Snackbar.make(aboutView, getString(R.string.not_yet_implemented), Snackbar.LENGTH_LONG).show());
+
+        mainMenuBinding.cardAboutApp.setOnClickListener(aboutView -> Snackbar.make(aboutView, getString(R.string.not_yet_implemented), Snackbar.LENGTH_LONG).show());
+        mainMenuBinding.cardAccountManager.setOnClickListener(aboutView -> Snackbar.make(aboutView, getString(R.string.not_yet_implemented), Snackbar.LENGTH_LONG).show());
+
+        mainMenuBinding.cardExpenses.setOnClickListener(expensesView -> ExNavigation.openExpensesHome(MainMenuActivity.this));
     }
+
+
 }
