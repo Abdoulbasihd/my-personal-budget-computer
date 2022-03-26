6d5c104995a304a1fa2cb1cb68b0ffc224f1beff
AbdoulBasihd <abdulbasihd@gmail.com>
2022-03-26 16:46:01 +0100

    about page ok

--- MyBudgetizer/app/src/main/java/cm/abimmobiledev/mybudgetizer/ui/MainMenuActivity.java
+++ MyBudgetizer/app/src/main/java/cm/abimmobiledev/mybudgetizer/ui/MainMenuActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.snackbar.Snackbar;
 
 import cm.abimmobiledev.mybudgetizer.R;
@@ -25,7 +27,11 @@ public class MainMenuActivity extends AppCompatActivity {
         mainMenuBinding.cardDebts.setOnClickListener(aboutView -> Snackbar.make(aboutView, getText(R.string.not_yet_implemented), Snackbar.LENGTH_LONG).show());
         mainMenuBinding.cardReceivable.setOnClickListener(aboutView -> Snackbar.make(aboutView, getString(R.string.not_yet_implemented), Snackbar.LENGTH_LONG).show());
 
-        mainMenuBinding.cardAboutApp.setOnClickListener(aboutView -> Snackbar.make(aboutView, getString(R.string.not_yet_implemented), Snackbar.LENGTH_LONG).show());
+        mainMenuBinding.cardAboutApp.setOnClickListener(aboutView -> {
+            BottomSheetDialog aboutBD = new BottomSheetDialog(MainMenuActivity.this);
+            aboutBD.setContentView(R.layout.bottom_sheet_about_app);
+            aboutBD.show();
+        });
         mainMenuBinding.cardAccountManager.setOnClickListener(aboutView -> Snackbar.make(aboutView, getString(R.string.not_yet_implemented), Snackbar.LENGTH_LONG).show());
 
         mainMenuBinding.cardExpenses.setOnClickListener(expensesView -> ExNavigation.openExpensesHome(MainMenuActivity.this));
